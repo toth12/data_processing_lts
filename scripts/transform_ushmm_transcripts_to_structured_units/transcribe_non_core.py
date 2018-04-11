9290548c5af34a999e81e9fa6ac9ece1f566a71b
gabrielsaruhashi <gabriel.saruhashi@gmail.com>
2018-04-11 11:58:01 -0400

    second version of transcribe_non_core docx, implemented backup methods for unstructured transcripts

--- scripts/transform_ushmm_transcripts_to_structured_units/transcribe_non_core.py
+++ scripts/transform_ushmm_transcripts_to_structured_units/transcribe_non_core.py
@@ -16,6 +16,89 @@ TRACKER = constants.TRACKER_COLLECTION
 OUTPUT = constants.OUTPUT_COLLECTION
 DB = constants.DB
 
+def safePrint(str_):
+    return ''.join(i for i in str_ if ord(i) < 128)
+
+def getMonologue(filename):
+    doc = Document(filename)
+    
+    monologue = ""
+    
+    # ignore all the -TITLE
+    # iterate over all paragraphs to get text units
+    for para in doc.paragraphs:
+        paragraph = para.text
+        
+        # ensure it is not an empty line
+        if paragraph:
+            
+            # ensure it is not a header type
+            if paragraph.count('-') < 2 and paragraph.count('.') < 2:
+                monologue += paragraph + ' '
+    
+    return list({'unit': monologue})
+
+def getUnstructuredUnits(filename):
+    doc = Document(filename)
+    units = list()
+    isHeader = True
+    for para in doc.paragraphs:
+        paragraph = para.text
+        
+        # ensure it is not an empty line
+        if paragraph:
+            # ignore the initial header info
+            if isHeader:
+                if paragraph.split() > 5:
+                    units.append(paragraph)
+                    isHeader = False
+
+            else:
+                units.append({'unit': paragraph})
+
+    return units   
+
+
+def get405Units(filename):
+    print(filename)
+    doc = Document(filename)
+    units = list()
+    isHeader = True
+    for para in doc.paragraphs:
+        paragraph = para.text
+        
+        # ensure it is not an empty line
+        if paragraph:
+            # check if there is a question
+            para_partition = paragraph.split(')', 1)
+            
+            
+            # it is a Q & A
+            if isHeader:
+                # check if we found the start of the interview - first question
+                if (len(para_partition) > 1 and 
+                    para_partition[0][len(para_partition[0]) - 1] == '?'):
+                    print(para_partition)
+                    # ignore the initial ? for the question
+                    units.append({'unit': para_partition[0][1:]})
+                    units.append({'unit:': para_partition[1]})
+                    isHeader = False
+            else:
+                if (len(para_partition) > 1 and 
+                    para_partition[0][len(para_partition[0]) - 1] == '?'):
+                    # ignore the initial ? for the question
+                    units.append({'unit': para_partition[0][1:]})
+                    units.append({'unit:': para_partition[1]})
+                else:
+                    units.append({'unit': paragraph})
+
+    # in case it is a monologue
+    #if not units:
+        #for para in doc.paragraphs:
+    if ".0031" in filename:
+        pprint.pprint(units)
+    return units
+
 def getTextUnits(filename):
     doc = Document(filename)
     units = list()
@@ -50,7 +133,8 @@ def getTextUnits(filename):
                 type1 or
                 "Answer:" in unit_type or 
                 typer2):
-                
+
+                safePrint(unit_type)
                 units.append({'unit': paragraph})
                 # update tracker
                 unit_tracker[unit_type] += 1
@@ -59,36 +143,53 @@ def getTextUnits(filename):
                     unit_type.lower() not in non_units and
                     unit_type[:-1].isalpha()):
                 
+                safePrint(unit_type)
                 units.append({'unit': paragraph})
                 # update tracker
                 unit_tracker[unit_type] += 1
             
             # backup method,in case it is in the format of e.g 'George Salton:'
-            elif len(paragraph.split()) > 1:
+            elif len(paragraph.split()) > 3:
                 backup_type = paragraph.split()[1]
-                #print(backup_type)
+                backup_two = paragraph.split()[2]
 
-                if ':' in backup_type and backup_type.lower() not in non_units: 
+                safePrint(backup_type)
+
+                if ((':' in backup_type and backup_type.lower() not in non_units) or
+                    (':' in backup_two and backup_two.lower() not in non_units)): 
                     units.append({'unit': paragraph})
                     # update tracker
                     unit_tracker[unit_type] += 1
-
-            elif len(paragraph.split()) > 2:
+            # "Marion Muscati:" or "Vicente del Bosque"
+            '''elif len(paragraph.split()) > 3:
                 backup_type = paragraph.split()[2]
+                third_backup = paragraph.split()[3]
                 
                 if filename == "RG-50.470.0012_trs_en.docx":
-                    print backup_type
-                if ':' in backup_type and backup_type.lower() not in non_units: 
+                    print third_backup
+                if ((':' in backup_type and backup_type.lower() not in non_units) or 
+                    (':' in third_backup and third_backup not in non_units)): 
                     units.append({'unit': paragraph})
                     # update tracker
-                    unit_tracker[unit_type] += 1
+                    unit_tracker[unit_type] += 1'''
  
 
     # check if backup method needed
-    if len(unit_tracker) == 1:
-        print(units)
-        print(filename)
-        return {}
+    if len(unit_tracker) < 2:
+        # the remaining 50.062 series is composed of monologues
+        if "RG-50.062" in filename:
+            units = getMonologue(filename)
+            return units
+        
+        elif "RG-50.233" in filename:
+            units = getUnstructuredUnits(filename)
+
+        elif "RG-50.405" in filename:
+            units = get405Units(filename)
+        
+        else:
+            return []
+
     return units
 
 def createStructuredTranscriptDocX():
@@ -121,7 +222,7 @@ def createStructuredTranscriptDocX():
             h.update_field(DB, TRACKER, "microsoft_doc_file", file, "status", "Processed")
         else:
             count += 1
-            #print(file)
+            print(file)
     print count
 
     # success
